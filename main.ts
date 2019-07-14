4f3bd17d3c769285348595e4db6ff24756c28453
WEREMSOFT <pablo.weremczuk@gmail.com>
2019-07-14 20:44:10 +0200

    Added examples on the event loop

--- main.ts
+++ main.ts
@@ -1,27 +1,59 @@
-import { BehaviorSubject, Observable } from 'rxjs/Rx';
+import { Subject, BehaviorSubject, Observable } from 'rxjs/Rx';
 import { createStore, Store } from 'redux';
 
-let bs: BehaviorSubject<string> = new BehaviorSubject<string>('Hello World!!');
+// 1. #### COLORS!!!
+//console.log('%c -=[BEGIN]=-', 'background: #000000; color: #00CC00');
 
-const ID: string  = '1';
+// 2. #### DIMPLE FUNCTION
 
-bs
-.map((value: string) => value.toUpperCase())
-.subscribe(value => console.log(value));
+// function hello(){
+//     console.log('Hello World!!');
+// }
 
-function myReducer(state: number = 0, action: any) {
-    switch(action.type) {
-        case 'INCREMENT': return state + 1;
-        case 'DECREMENT': return state - 1;
-        default:
-            return state;
-    }
+// 2. #### What is the Event Loop?
 
-}
+// setTimeout(hello, 0);
 
-let store: Store = createStore(myReducer);
+// #1 The main execution thread
+// #2 Waiting Queue
+// #3 Ready Queue
 
-// store.subscribe( subscribe( (value) => console.log(store.getState(), store. , value));
+// 3. #### What about observables? In or out the vent loop?
+// let bs: Subject<string> = new Subject<string>();
+// bs.subscribe(data => console.log('from observable: ' + data));
+// bs.next('test');
 
-store.dispatch({type: 'INCREMENT', id: ID});
+// 4. #### Observables are Sync EVENT IF WE HAVE A LOT OF SUBSCRIBERS!!!
+// let bs: Subject<string> = new Subject<string>();
+// bs.subscribe(data => console.log('from observable 1: ' + data));
+// bs.subscribe(data => console.log('from observable 2: ' + data));
+// bs.subscribe(data => console.log('from observable 3: ' + data));
+// bs.subscribe(data => console.log('from observable 4: ' + data));
+// bs.subscribe(data => console.log('from observable 5: ' + data));
+
+// bs.next('test 1');
+// bs.next('test 2');
+// bs.next('test 3');
+// bs.next('test 4');
+// bs.next('test 5');
+// bs.next('test 6');
+
+// 5. #### We can turn an observable into a promise
+
+// console.log('%c -=[BEGIN]=-', 'background: #000000; color: #00CC00');
+
+// let bs: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+
+// bs.subscribe(data => console.log('subscription: ' + data));
+
+// bs.next('MyData');
+
+// bs.toPromise().then(data => console.log('from promise: ' + data));
+
+// bs.complete();
+
+// console.log('%c -=[END]=-', 'background: #000000; color: #00CC00');
+
+
 
