2214f0a2b601418a5d9da6ec8711d4d5ea38e9ef
WEREMSOFT <pablo.weremczuk@gmail.com>
2019-08-29 21:34:21 +0200

    some example code for web componetns

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+class HelloWorld extends HTMLElement {
+    constructor() {
+        super();
+        this.template = document.createElement('template');
+        this.template.innerHTML = '<h1>hello world</h1><slot/>';
+        console.log('hello from constructor');
+        this.attachShadow({
+            mode: 'open'
+        });
+    }
+
+    connectedCallback() {
+        this.shadowRoot.appendChild(this.template.content);
+        console.log('connected callback', this.getAttribute('cadorna'));
+    }
+
+
+}
+
+customElements.define('hello-world', HelloWorld);
+
